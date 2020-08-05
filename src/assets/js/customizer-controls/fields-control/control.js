11d90f6c4016d041e2d74ff18ef7397c4880cc0e
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2020-08-05 16:49:06 +0100

    added countries to select option in campaign

--- src/assets/js/customizer-controls/fields-control/control.js
+++ src/assets/js/customizer-controls/fields-control/control.js
@@ -26,6 +26,25 @@
                         }
                     };
 
+                    var maybeHideCustomOptionsField = function () {
+                        var field_type = $(field).find('.mo-optin-fields-field').val();
+                        if(field_type === 'custom_select') {
+                            $(field).find(".list_custom_field_options.mo-fields-block").show();
+                        } else {
+                            $(field).find(".list_custom_field_options.mo-fields-block").hide();
+                        }
+                    }
+
+                    var maybeHideCountryOptionsField = function () {
+                        var field_type = $(field).find('.list_custom_field_options .mo-optin-fields-field').val();
+                        console.log(field_type);
+                        if(field_type === 'country') {
+                            $(field).find(".list_country_options_field_options.mo-fields-block").show();
+                        } else {
+                            $(field).find(".list_country_options_field_options.mo-fields-block").hide();
+                        }
+                    }
+
                     var maybeHideListSubscriptionFields = function () {
                         var field_type = $(field).find('.mo-optin-fields-field').val();
                         if (field_type === 'list_subscription') {
@@ -98,6 +117,8 @@
                     };
 
                     maybeHideOptionsField();
+                    maybeHideCustomOptionsField();
+                    maybeHideCountryOptionsField();
                     maybeHideRecaptchaField();
                     maybeHideHiddenValueField();
                     maybeHideListSubscriptionFields();
@@ -107,6 +128,8 @@
                         .off('change.mo_field')
                         .on('change.mo_field', function () {
                             maybeHideOptionsField();
+                            maybeHideCustomOptionsField();
+                            maybeHideCountryOptionsField();
                             maybeHideRecaptchaField();
                             maybeHideHiddenValueField();
                             maybeHideListSubscriptionFields();
