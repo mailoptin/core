f0cfdf788d5dfc85ec93a4b5f4867218c0152c87
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2021-01-24 12:40:10 +0100

    initiate class to import leads

---
+++ src/Admin/SettingsPage/ConversionImport.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MailOptin\Core\Admin\SettingsPage;
+
+
+use League\Csv\Writer;
+use MailOptin\Core\Repositories\OptinCampaignsRepository;
+use MailOptin\Core\Repositories\OptinConversionsRepository;
+
+class ConversionImport {
+
+}
