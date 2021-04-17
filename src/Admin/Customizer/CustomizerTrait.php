0db8545555b1b4c01dc87ac2b6dda1b76074713a
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2021-04-17 18:25:30 +0100

    webhook label change

--- src/Admin/Customizer/CustomizerTrait.php
+++ src/Admin/Customizer/CustomizerTrait.php
@@ -247,6 +247,7 @@ trait CustomizerTrait
      */
     public function js_script()
     {
+        $webhook_label     = __('Request Method (Required)', 'mailoptin');
         $wp_user_reg_label = __('Select User Role (Required)', 'mailoptin');
         $ck_label          = __('ConvertKit Forms', 'mailoptin');
         $drip_label        = __('Drip Campaigns', 'mailoptin');
@@ -292,6 +293,10 @@ trait CustomizerTrait
                         if (connection_service === 'WordPressUserRegistrationConnect') {
                             title_obj.text('<?php echo $wp_user_reg_label; ?>');
                         }
+
+                        if (connection_service === 'WebHookConnect') {
+                            title_obj.text('<?php echo $webhook_label; ?>');
+                        }
                     }
 
                     function logic_new(connection_service, parent) {
@@ -329,6 +334,10 @@ trait CustomizerTrait
                                 if (connection_service === 'WordPressUserRegistrationConnect') {
                                     title_obj.text('<?php echo $wp_user_reg_label; ?>');
                                 }
+
+                                if (connection_service === 'WebHookConnect') {
+                                    title_obj.text('<?php echo $webhook_label; ?>');
+                                }
                             });
                         } else {
 
@@ -362,6 +371,10 @@ trait CustomizerTrait
 
                             if (connection_service === 'WordPressUserRegistrationConnect') {
                                 title_obj.text('<?php echo $wp_user_reg_label; ?>');
+                            }
+
+                            if (connection_service === 'WebHookConnect') {
+                                title_obj.text('<?php echo $webhook_label; ?>');
                             }
                         }
                     }
