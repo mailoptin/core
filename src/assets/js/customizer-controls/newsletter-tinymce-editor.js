0328a89c1d2fe0c6bfc974fa5efe8b10a2bb9c56
Collins Agbonghama <collizo4sky@gmail.com>
2021-10-05 13:29:14 +0100

    Fixed bug where tinymce control in customizer wasn’t triggering change events

--- src/assets/js/customizer-controls/newsletter-tinymce-editor.js
+++ src/assets/js/customizer-controls/newsletter-tinymce-editor.js
@@ -2,25 +2,29 @@
 
     $(window).on('load', function () {
 
-        if (typeof tinymce === 'undefined') return;
+        setTimeout(function () {
 
-        var editor = tinymce.get('mo_newsletter_editor');
+            if (typeof tinymce === 'undefined') return;
 
-        if (editor) {
-            editor.on('keyup change undo redo SetContent Paste', function () {
-                editor.save();
-                parent.wp.customize.value(mailoptin_email_campaign_option_prefix + '[' + mailoptin_email_campaign_id + '][newsletter_editor_content]').set(
-                    editor.getContent()
-                );
-            });
-        }
+            var editor = tinymce.get('mo_newsletter_editor');
 
-        $(document).on('change keyup', '#mo_newsletter_editor', _.debounce(function () {
-                parent.wp.customize.value(mailoptin_email_campaign_option_prefix + '[' + mailoptin_email_campaign_id + '][newsletter_editor_content]').set(
-                    $(this).val()
-                );
-            }, 300)
-        );
+            if (editor) {
+                editor.on('keyup change undo redo SetContent Paste', function () {
+                    editor.save();
+                    parent.wp.customize.value(mailoptin_email_campaign_option_prefix + '[' + mailoptin_email_campaign_id + '][newsletter_editor_content]').set(
+                        editor.getContent()
+                    );
+                });
+            }
+
+            $(document).on('change keyup', '#mo_newsletter_editor', _.debounce(function () {
+                    parent.wp.customize.value(mailoptin_email_campaign_option_prefix + '[' + mailoptin_email_campaign_id + '][newsletter_editor_content]').set(
+                        $(this).val()
+                    );
+                }, 300)
+            );
+
+        }, 1000);
     });
 
 })(jQuery);
--- src/assets/js/customizer-controls/tinymce-control.js
+++ src/assets/js/customizer-controls/tinymce-control.js
@@ -3,32 +3,37 @@
     var Tinymce_Customize_Control = {
 
         init: function () {
+
             $(window).on('load', function () {
 
-                $('textarea.wp-editor-area').each(function () {
-                    var tArea = $(this),
-                        id = tArea.attr('id'),
-                        editor = tinymce.get(id),
-                        content;
+                setTimeout(function () {
 
-                    if (editor) {
-                        editor.on('keyup change undo redo SetContent Paste', function () {
-                            editor.save();
-                            content = editor.getContent();
-                            tArea.val(content).trigger('change');
+                    $('textarea.wp-editor-area').each(function () {
+                        var tArea = $(this),
+                            id = tArea.attr('id'),
+                            editor = tinymce.get(id),
+                            content;
 
-                            // if there is a shortcode embed, refresh the preview
-                            if ((new RegExp(/\[.+\]/g)).test(content) === true) {
-                                wp.customize.previewer.refresh();
-                            }
-                        });
-                    }
-                }).on('keyup change', function () {
-                    // if there is a shortcode embed, refresh the preview
-                    if ((new RegExp(/\[.+\]/g)).test(this.value) === true) {
-                        wp.customize.previewer.refresh();
-                    }
-                });
+                        if (editor) {
+                            editor.on('keyup change undo redo SetContent Paste', function () {
+                                editor.save();
+                                content = editor.getContent();
+                                tArea.val(content).trigger('change');
+
+                                // if there is a shortcode embed, refresh the preview
+                                if ((new RegExp(/\[.+\]/g)).test(content) === true) {
+                                    wp.customize.previewer.refresh();
+                                }
+                            });
+                        }
+                    }).on('keyup change', function () {
+                        // if there is a shortcode embed, refresh the preview
+                        if ((new RegExp(/\[.+\]/g)).test(this.value) === true) {
+                            wp.customize.previewer.refresh();
+                        }
+                    });
+
+                }, 1000);
             });
         }
     };
