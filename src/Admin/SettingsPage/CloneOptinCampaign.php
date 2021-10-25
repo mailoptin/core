eb7854d6b3b4bcba30799ef404486de373b25ace
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2021-10-25 23:36:31 +0100

    Replace Old UUID in custom css with the new UUID

--- src/Admin/SettingsPage/CloneOptinCampaign.php
+++ src/Admin/SettingsPage/CloneOptinCampaign.php
@@ -25,18 +25,23 @@ class CloneOptinCampaign
     public function forge()
     {
         $clonee = OptinCampaignsRepository::get_optin_campaign_by_id($this->optin_campaign_id);
+        $new_uuid = AjaxHandler::generateUniqueId();
 
         $optin_campaign_id = OptinCampaignsRepository::add_optin_campaign(
-            AjaxHandler::generateUniqueId(),
+            $new_uuid,
             apply_filters('mailoptin_optin_campaign_clone_name', $clonee['name'] . ' - Copy', $clonee),
             $clonee['optin_class'],
             $clonee['optin_type']
         );
 
         $all_optin_campaign_settings = OptinCampaignsRepository::get_settings();
+        
         // append new template settings to existing settings.
         $all_optin_campaign_settings[$optin_campaign_id] = OptinCampaignsRepository::get_settings_by_id($this->optin_campaign_id);
-
+        
+        //replace all the strings uuid with the new uuid in the custom css
+        $all_optin_campaign_settings[$optin_campaign_id]["form_custom_css"] = str_replace('#'.$clonee['uuid'], '#'.$new_uuid, $all_optin_campaign_settings[$optin_campaign_id]["form_custom_css"]);
+        
         // deactivate cloned optin campaign by default
         $all_optin_campaign_settings[$optin_campaign_id]['activate_optin'] = false;
 
