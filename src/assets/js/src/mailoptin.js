d2c131bb85e06343cc439a9fb47caeced7745bbc
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2021-11-05 21:49:30 +0100

    revert mailoptin.js file

--- src/assets/js/src/mailoptin.js
+++ src/assets/js/src/mailoptin.js
@@ -1330,31 +1330,26 @@ var mailoptin_optin = {
 
         if (!mailoptin_optin.is_content_locker_enabled(optin_config)) return;
 
-        var nextAll2 = false,
-            contentSelector = optin_config.content_lock_selector.toString(),
-            nextAll = $('#' + optin_config.optin_uuid).nextAll();
+        var contentSelector = optin_config.content_lock_selector.toString();
 
-        if (contentSelector.length > 0 && $(contentSelector).length > 0) {
-            nextAll2 = $(contentSelector);
+        var nextAll = $('#' + optin_config.optin_uuid).nextAll();
+
+        if(contentSelector.length > 0 && $(contentSelector).length > 0) {
+            nextAll = $(contentSelector);
         }
 
         if ('removal' === optin_config.content_lock_style) {
-
-            if (nextAll2 !== false) {
-                nextAll2.hide();
+            if($(contentSelector).length > 0) {
+                nextAll.each(function (index, el) {
+                    $(el).hide();
+                });
+            } else {
+                nextAll.each(function (index, el) {
+                    mailoptin_optin.content_locker_storage.push($(el).clone(true));
+                    $(el).remove();
+                });
             }
-
-            nextAll.each(function (index, el) {
-                mailoptin_optin.content_locker_storage.push($(el).clone(true));
-                $(el).remove();
-            });
-
         } else {
-
-            if (nextAll2 !== false) {
-                nextAll2.addClass('mailoptin-content-lock');
-            }
-
             nextAll.each(function (index, el) {
                 $(el).addClass('mailoptin-content-lock');
             });
@@ -1366,32 +1361,32 @@ var mailoptin_optin = {
         if (!mailoptin_optin.is_content_locker_enabled(optin_config)) return;
 
         var optin_container = $('#' + optin_config.optin_uuid),
-            nextAll2 = false,
-            nextAll = optin_container.nextAll(),
-            contentSelector = optin_config.content_lock_selector.toString();
+            nextAll = optin_container.nextAll();
 
-        if (contentSelector.length > 0 && $(contentSelector).length > 0) {
-            nextAll2 = $(contentSelector);
+        var contentSelector = optin_config.content_lock_selector.toString();
+
+        if(contentSelector.length > 0 && $(contentSelector).length > 0) {
+            nextAll = $(contentSelector);
         }
 
         if ('removal' === optin_config.content_lock_style) {
 
             mailoptin_optin.content_locker_storage.reverse();
 
-            if (nextAll2 !== false) {
-                nextAll2.show();
+            if($(contentSelector).length > 0) {
+                nextAll.each(function (index, el) {
+                    $(el).show();
+                });
+            } else {
+                $.each(mailoptin_optin.content_locker_storage, function (index, el) {
+                    optin_container.after(el);
+                });
             }
-
-            $.each(mailoptin_optin.content_locker_storage, function (index, el) {
-                optin_container.after(el);
-            });
-
         } else {
-            nextAll.removeClass('mailoptin-content-lock');
 
-            if (nextAll2 !== false) {
-                nextAll2.removeClass('mailoptin-content-lock');
-            }
+            nextAll.each(function (index, el) {
+                $(el).removeClass('mailoptin-content-lock');
+            });
         }
     },
 
