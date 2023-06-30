b1bcac76735d63aade490f58a61b1ecceaf4b488
Collins Agbonghama <collizo4sky@gmail.com>
2023-06-30 15:50:41 +0100

    oceanwp conflict: bug where range slider didn’t work

--- src/assets/js/customizer-controls/range-value-control/customizer-range-value-control.js
+++ src/assets/js/customizer-controls/range-value-control/customizer-range-value-control.js
@@ -1,28 +1,39 @@
 /**
  * Script run inside a Customizer control sidebar
  */
-(function($) {
-    wp.customize.bind('ready', function() {
+(function ($) {
+
+    wp.customize.bind('ready', function () {
         rangeSlider();
     });
 
-    var rangeSlider = function() {
+    var valueFormatter = function () {
+
+        window.crvc_init = false;
+
+        if (window.crvc_init === true) return;
+
+        window.crvc_init = true;
+
         var slider = $('.range-slider'),
-            range = $('.range-slider__range'),
             value = $('.range-slider__value');
 
-        slider.each(function() {
+        slider.each(function () {
 
-            value.each(function() {
+            value.each(function () {
                 var value = $(this).prev().attr('value');
-				var suffix = ($(this).prev().attr('suffix')) ? $(this).prev().attr('suffix') : '';
+                var suffix = ($(this).prev().attr('suffix')) ? $(this).prev().attr('suffix') : '';
                 $(this).html(value + suffix);
             });
+        });
+    };
 
-            range.on('input', function() {
-				var suffix = ($(this).attr('suffix')) ? $(this).attr('suffix') : '';
-                $(this).next(value).html(this.value + suffix );
-            });
+    var rangeSlider = function () {
+
+        $(document).on('input', '.range-slider__range', function () {
+            valueFormatter();
+            var suffix = ($(this).attr('suffix')) ? $(this).attr('suffix') : '';
+            $(this).next('.range-slider__value').html(this.value + suffix);
         });
     };
 
