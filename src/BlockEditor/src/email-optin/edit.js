bcc1cb577fc65388876ea2ee3a94f98a3d117a6e
Collins Agbonghama <collizo4sky@gmail.com>
2024-06-11 17:52:53 +0100

    wip

--- src/BlockEditor/src/email-optin/edit.js
+++ src/BlockEditor/src/email-optin/edit.js
@@ -1,41 +1,58 @@
 import {__} from '@wordpress/i18n';
-import {useBlockProps} from '@wordpress/block-editor';
-import {SelectControl} from '@wordpress/components';
+import {
+    SelectControl,
+    Notice,
+    Panel,
+    PanelBody,
+    PanelRow
+} from '@wordpress/components';
+
+import {
+    useBlockProps,
+    ColorPalette,
+    InspectorControls,
+} from '@wordpress/block-editor';
+
 import './editor.scss';
 
 const optin_options = Object.entries(moBlockOptinCampaigns.optins).map(([id, label]) => {
     return {label, value: id}
 });
 
-function FormSelectField({attributes, setAttributes}) {
-
+function EditorContent({attributes, setAttributes}) {
     return (
         <div className="mailoptin-form-block-select-wrap">
             {
                 optin_options.length === 0 ?
-                    (__('No optin campaign found. Please create one', 'mailoptin')) :
-                    <SelectControl
-                        label={__('Select an optin campaign to display', 'mailoptin')}
-                        options={[
-                            {label: "––––"},
-                            ...optin_options
-                        ]}
-                        onChange={(id) => setAttributes({id})}
-                        value={attributes.id}
-                    />
+                    <Notice status="error" isDismissible={false}>{__('No optin campaign found. Please create one', 'mailoptin')}</Notice> :
+                    <OptinSelectField attributes={attributes} setAttributes={setAttributes}/>
             }
         </div>
     );
 }
 
+function OptinSelectField({attributes, setAttributes}) {
+    return <SelectControl
+        label={__('Select an optin campaign', 'mailoptin')}
+        options={[
+            {label: "––––"},
+            ...optin_options
+        ]}
+        onChange={(id) => setAttributes({id})}
+        value={attributes.id}
+    />
+}
+
 export default function Edit({attributes, setAttributes}) {
     return (
         <div {...useBlockProps()}>
-            {
-                (!attributes.id) ?
-                    <FormSelectField setAttributes={setAttributes} attributes={attributes}/> :
-                    'hello'
-            }
+            <InspectorControls key="setting"> <Panel> <PanelBody initialOpen={true} title={__('Optin Campaign', 'mailoptin')}>
+                <PanelRow> <OptinSelectField attributes={attributes} setAttributes={setAttributes}/> </PanelRow>
+            </PanelBody> </Panel> </InspectorControls> {
+            (optin_options.length === 0 || !attributes.id) ?
+                <EditorContent setAttributes={setAttributes} attributes={attributes}/> :
+                'hello'
+        }
         </div>
     );
 }
